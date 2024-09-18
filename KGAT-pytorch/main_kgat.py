bb4cceb37ba31d77447cb57e69ffb09745fdf938
1645 <e1348779@e.nus.edu>
2024-09-18 08:54:22 +0800

    change model

--- KGAT-pytorch/main_kgat.py
+++ KGAT-pytorch/main_kgat.py
@@ -226,8 +226,8 @@ def predict(args):
     Ks = eval(args.Ks)
     k_min = min(Ks)
     k_max = max(Ks)
-
     cf_scores, metrics_dict = evaluate(model, data, Ks, device)
+    
     np.save(args.save_dir + 'cf_scores.npy', cf_scores)
     print('CF Evaluation: Precision [{:.4f}, {:.4f}], Recall [{:.4f}, {:.4f}], NDCG [{:.4f}, {:.4f}]'.format(
         metrics_dict[k_min]['precision'], metrics_dict[k_max]['precision'], metrics_dict[k_min]['recall'], metrics_dict[k_max]['recall'], metrics_dict[k_min]['ndcg'], metrics_dict[k_max]['ndcg']))
@@ -236,7 +236,7 @@ def predict(args):
 
 if __name__ == '__main__':
     args = parse_kgat_args()
-    train(args)
-    # predict(args)
+    # train(args)
+    predict(args)
 
 
--- README.md
+++ README.md
@@ -5,8 +5,15 @@
 ### 2.1 pdf/word Resume to picture
 ### 2.2 OCR recognizes text in pictures
 ### 2.3 Text processing, extract keywords
-## Section3 [Liu Zimu]KGAT
+## Section3 [Liu Zimu]Recommendation System
 ### 3.1 KGAT Implementation
+#### 3.1.1 Model Input
+* `kg_final.txt`: the final knowledge graph
+* `test.txt`: the test data
+* `train.txt`: the training data
+#### 3.1.2 Model Output
+* `cf_scores.npy`: Recommendation for each item by each user
 ### 3.2 New user recommendation
+
 ## Section4 [Feng Yuan]Web Development
 ### 4.1 Web UI
