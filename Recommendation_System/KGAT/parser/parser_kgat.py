4d374b732cd2167593748b0c11ae284964f90843
1645 <lzm1645lzm@gmail.com>
2024-10-22 22:26:15 +0800

    Refactor code to enable prediction in main_kgat.py

--- Recommendation_System/KGAT/parser/parser_kgat.py
+++ Recommendation_System/KGAT/parser/parser_kgat.py
@@ -16,14 +16,14 @@ def parse_kgat_args():
                         help='0: No pretrain, 1: Pretrain with the learned embeddings, 2: Pretrain with stored model.')
     parser.add_argument('--pretrain_embedding_dir', nargs='?', default='datasets/pretrain/',
                         help='Path of learned embeddings.')
-    parser.add_argument('--pretrain_model_path', nargs='?', default='trained_model/model.pth',
+    parser.add_argument('--pretrain_model_path', nargs='?', default='trained_model/KGAT/ours/pretrain0/model_epoch7.pth',
                         help='Path of stored model.')
 
-    parser.add_argument('--cf_batch_size', type=int, default=1024,
+    parser.add_argument('--cf_batch_size', type=int, default=8,
                         help='CF batch size.')
-    parser.add_argument('--kg_batch_size', type=int, default=2048,
+    parser.add_argument('--kg_batch_size', type=int, default=64,
                         help='KG batch size.')
-    parser.add_argument('--test_batch_size', type=int, default=10000,
+    parser.add_argument('--test_batch_size', type=int, default=10,
                         help='Test batch size (the user number to test every batch).')
 
     parser.add_argument('--embed_dim', type=int, default=64,
@@ -40,12 +40,12 @@ def parse_kgat_args():
     parser.add_argument('--mess_dropout', nargs='?', default='[0.1, 0.1, 0.1]',
                         help='Dropout probability w.r.t. message dropout for each deep layer. 0: no dropout.')
 
-    parser.add_argument('--kg_l2loss_lambda', type=float, default=1e-5,
+    parser.add_argument('--kg_l2loss_lambda', type=float, default=1e-6,
                         help='Lambda when calculating KG l2 loss.')
-    parser.add_argument('--cf_l2loss_lambda', type=float, default=1e-5,
+    parser.add_argument('--cf_l2loss_lambda', type=float, default=1e-6,
                         help='Lambda when calculating CF l2 loss.')
 
-    parser.add_argument('--lr', type=float, default=0.0001,
+    parser.add_argument('--lr', type=float, default=0.00001,
                         help='Learning rate.')
     parser.add_argument('--n_epoch', type=int, default=1000,
                         help='Number of epoch.')
@@ -56,7 +56,7 @@ def parse_kgat_args():
                         help='Iter interval of printing CF loss.')
     parser.add_argument('--kg_print_every', type=int, default=1,
                         help='Iter interval of printing KG loss.')
-    parser.add_argument('--evaluate_every', type=int, default=10,
+    parser.add_argument('--evaluate_every', type=int, default=1,
                         help='Epoch interval of evaluating CF.')
 
     parser.add_argument('--Ks', nargs='?', default='[20, 40, 60, 80, 100]',
